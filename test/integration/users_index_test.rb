d7b06aaae68f9157a12f60c1312733e10b998255
Mark Newman <markedwardnewman@gmail.com>
2015-11-17 09:49:00 +0000

    Comment change

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -33,7 +33,7 @@ end
 
 # see https://www.railstutorial.org/book/updating_and_deleting_users#sec-unsuccessful_edits
 #   Listing 9.57: An integration test for delete links and destroying users.
-# as to why the below has been commented out
+# as to why the below has been commented out yep
   # test "index including pagination" do
   #   log_in_as(@user)
   #   get users_path
